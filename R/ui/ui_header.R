9ebf81bc0f387227d7f96627e5e3644d780da61c
Mark <altosaar@gmail.com>
2026-02-11 13:11:41 -0500

    first pass at style changes

--- R/ui/ui_header.R
+++ R/ui/ui_header.R
@@ -4,8 +4,8 @@
 create_app_header <- function() {
   div(class = "app-header",
     div(class = "header-content",
-      h1("Survey Explorer", class = "app-title"),
-      p("Explore survey responses with interactive analysis", class = "app-subtitle")
+      h1("Online Pathways - Survey Analysis", class = "app-title"),
+      p("Responses collected anonymously between November 24 - December 24, 2025", class = "app-subtitle")
     )
   )
 }
