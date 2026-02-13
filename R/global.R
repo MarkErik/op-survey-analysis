cee476ed1479efeb6234239ae55225bf296011ce
Mark <altosaar@gmail.com>
2026-02-12 20:36:53 -0500

    refactor: update global.R to use new data pipeline

--- R/global.R
+++ R/global.R
@@ -1,48 +1,51 @@
 # Global variables and data loading
 
-# Define free-text questions
-free_text_questions <- c(
-  "free_text_learning_preference" = "Learning Preference",
-  "free_text_challenge_meeting_people" = "Challenge Meeting People",
-  "free_text_class_welcoming_inclusive" = "Class Welcoming & Inclusive",
-  "free_text_class_interesting_engaging" = "Class Interesting & Engaging",
-  "free_text_learning_meeting_expectations" = "Learning Meeting Expectations",
-  "free_text_hopes_interacting_students" = "Hopes Interacting with Students",
-  "free_text_hopes_interacting_professor" = "Hopes Interacting with Professor",
-  "free_text_class_favorite_part" = "Class Favorite Part",
-  "free_text_class_least_enjoyable_part" = "Class Least Enjoyable Part",
-  "free_text_more_and_less_of" = "More and Less Of",
-  "discord_other_text" = "Discord Other Text"
+# Load required packages
+library(dplyr)
+library(stringr)
+library(tidyr)
+
+# Initialize global column mappings
+column_mappings <- list(
+  original = character(0),
+  normalized = character(0)
 )
 
-# Define standardized column names for survey questions
-# These match the lowercase-converted column names from the CSV file
-survey_columns <- list(
-  course = c(
-    excited = "x.course..i.am.excited.about.the.content.and.material.that.i.m.learning",
-    relevant = "x.course..the.content.is.relevant.and.up.to.date",
-    meeting_goals = "x.course..i.feel.like.i.am.meeting.the.goals.of.learning.python.in.this.course",
-    apply_scenario = "x.course..i.feel.like.i.could.take.what.i.m.learning.and.apply.it.in.a.new.scenario",
-    feedback = "x.course..i.m.satisfied.with.the.level.of.feedback.i.receive",
-    ask_help = "x.course..it.s.easy.to.ask.for.help"
-  ),
-  learning = c(
-    pre_written_code = "x.learning..explanations.of.pre.written.code",
-    live_coding = "x.learning..live.coding.by.the.professor",
-    slides = "x.learning..presentation.slides",
-    handouts = "x.learning..post.class.handouts.and.notes",
-    tophat_quizzes = "x.learning..tophat.quizzes",
-    assignments = "x.learning..assignments",
-    labs = "x.learning..labs",
-    ask_questions = "x.learning..being.able.to.ask.questions.of.the.professor.during.lecture",
-    studying_midterms = "x.learning..studying.for.midterms",
-    coding_own = "x.learning..coding.on.my.own"
-  ),
-  community = c(
-    friends_important = "x.community..making.friends.within.the.class.is.important.to.me",
-    easy_meet = "x.community..it.s.easy.to.meet.new.people.within.the.class",
-    part_of_class = "x.community..i.feel.like.i.am.a.part.of.this.class",
-    comfortable_speaking = "x.community..i.feel.comfortable.speaking.up.in.class",
-    part_of_university = "x.community..i.feel.like.i.am.a.part.of.the.university.community"
-  )
+# Source data modules
+source("R/data/data_import.R")
+source("R/data/data_processing.R")
+source("R/data/data_validation.R")
+source("R/data/data_access.R")
+
+# Load and process survey data at app startup
+raw_data <- load_survey_data()
+
+if (!is.null(raw_data)) {
+  survey_data <- process_survey_data(raw_data)
+  
+  # Validate processed data
+  validation_result <- validate_survey_data(survey_data)
+  log_validation_errors(validation_result)
+  
+  if (!validation_result$valid) {
+    message("WARNING: Data validation failed. Some features may not work correctly.")
+  }
+} else {
+  survey_data <- NULL
+  message("ERROR: Failed to load survey data. Application may not function correctly.")
+}
+
+# Define free-text questions for reference
+free_text_questions <- c(
+  "how_is_the_course_meeting_your_expectations_for_what_you_hoped_to_learn_or_experience_optional",
+  "why_is_this_your_preferred_way_of_learning",
+  "optional_if_you_re_not_taking_this_class_in_your_preferred_learning_method_why",
+  "thinking_about_what_helps_you_learn_the_best",
+  "what_s_been_your_favorite_part_of_the_class_optional",
+  "what_s_been_the_least_enjoyable_part_optional",
+  "what_s_the_greatest_challenge_in_meeting_new_people_optional",
+  "please_remark_on_aspects_of_the_class_that_make_it_welcoming_optional",
+  "what_were_your_expectations_hopes_for_interacting_with_the_other_students_optional",
+  "what_were_your_expectations_hopes_for_interacting_with_the_professor_optional",
+  "any_other_comments_optional"
 )
